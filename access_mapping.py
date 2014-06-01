2f8b2de76fa5aaa46c3f200fe94620210de74575
Nicco Kunzmann <niccokunzmann@googlemail.com>
2014-06-01 12:08:32 +0200

    corrected list access

--- access_mapping.py
+++ access_mapping.py
@@ -27,11 +27,11 @@ def get_access_mapping(cls, default = default_access):
         return access_mapping[cls]
     return default
 
-map(list, ['__add__', '__contains__', '__dir__', '__doc__', '__eq__',
+map(list, ['__add__', '__contains__', '__dir__', '__eq__',
            '__format__', '__ge__', '__getitem__', '__gt__', '__hash__',
            '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__',
            '__reduce__', '__reduce_ex__', '__repr__', '__reversed__',
-           '__rmul__', '__sizeof__', '__str__', 'append', 'copy', 'count',
+           '__rmul__', '__sizeof__', '__str__', 'copy', 'count',
            'index'], ['__class__', '__doc__', '__subclasshook__'])
 
 map(set, ['__and__', '__contains__', '__dir__', '__eq__', '__format__',
